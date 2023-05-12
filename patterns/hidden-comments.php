9d5eb2b15d3f13ff77c28097527d09725843ab28
UladzimirKulesh <u.a.kuesh@gmail.com>
2023-05-12 18:09:14 +0200

    Working

--- patterns/hidden-comments.php
+++ patterns/hidden-comments.php
@@ -9,39 +9,38 @@
 <div class="wp-block-group">
 	<!-- wp:comments {"style":{"spacing":{"margin":{"top":"var:preset|spacing|60"}}}} -->
 	<div class="wp-block-comments" style="margin-top:var(--wp--preset--spacing--60)">
-		<!-- wp:heading {"level":2,"style":{"spacing":{"margin":{"top":"0"}}}} -->
-		<h2 style="margin-top:0"><?php echo esc_html_x( 'Comments', 'Title of comments section', 'silence' ); ?></h2>
+		<!-- wp:heading {"style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|10"}}}} -->
+		<h2 class="wp-block-heading" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--10)"><?php echo esc_html_x( 'Comments', 'Title of comments section', 'silence' ); ?></h2>
 		<!-- /wp:heading -->
 
 		<!-- wp:comments-title {"level":3} /-->
 
 		<!-- wp:comment-template -->
-			<!-- wp:columns {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40"}}}} -->
-			<div class="wp-block-columns" style="margin-bottom:var(--wp--preset--spacing--40)">
-				<!-- wp:column {"width":"40px"} -->
-				<div class="wp-block-column" style="flex-basis:40px">
-					<!-- wp:avatar {"size":40,"style":{"border":{"radius":"20px"}}} /-->
-				</div>
-				<!-- /wp:column -->
+			<!-- wp:group {"tagName":"article","style":{"border":{"top":{"color":"var:preset|color|contrast-25","width":"1px"}},"spacing":{"padding":{"top":"var:preset|spacing|30"},"margin":{"bottom":"var:preset|spacing|30"}}},"className":"comment-body","layout":{"type":"constrained"}} -->
+			<article class="wp-block-group comment-body" style="border-top-color:var(--wp--preset--color--contrast-25);border-top-width:1px;margin-bottom:var(--wp--preset--spacing--30);padding-top:var(--wp--preset--spacing--30)">
+				<!-- wp:group {"tagName":"footer","style":{"spacing":{"blockGap":"1.5rem"}},"className":"comment-meta","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+				<footer class="wp-block-group comment-meta">
+					<!-- wp:avatar {"size":48,"style":{"border":{"radius":"50%"}}} /-->
 
-				<!-- wp:column -->
-				<div class="wp-block-column">
-					<!-- wp:comment-author-name /-->
+					<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+					<div class="wp-block-group">
+						<!-- wp:comment-author-name /-->
 
-					<!-- wp:group {"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"flex"}} -->
-					<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px">
-						<!-- wp:comment-date /-->
-						<!-- wp:comment-edit-link /-->
+						<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+						<div class="wp-block-group">
+							<!-- wp:comment-date /-->
+							<!-- wp:comment-edit-link /-->
+						</div>
+						<!-- /wp:group -->
 					</div>
 					<!-- /wp:group -->
+				</footer>
+				<!-- /wp:group -->
 
-					<!-- wp:comment-content /-->
-
-					<!-- wp:comment-reply-link /-->
-				</div>
-				<!-- /wp:column -->
-			</div>
-			<!-- /wp:columns -->
+				<!-- wp:comment-content {"className":"comment-content"} /-->
+				<!-- wp:comment-reply-link {"className":"comment-reply"} /-->
+			</article>
+			<!-- /wp:group -->
 		<!-- /wp:comment-template -->
 
 		<!-- wp:comments-pagination {"paginationArrow":"arrow","layout":{"type":"flex","justifyContent":"space-between"}} -->
@@ -50,7 +49,7 @@
 			<!-- wp:comments-pagination-next /-->
 		<!-- /wp:comments-pagination -->
 
-	<!-- wp:post-comments-form /-->
+		<!-- wp:post-comments-form {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50"}}}} /-->
 	</div>
 	<!-- /wp:comments -->
 </div>
--- patterns/hidden-no-results.php
+++ patterns/hidden-no-results.php
@@ -5,10 +5,12 @@
  * Inserter: no
  */
 ?>
-<!-- wp:paragraph -->
-<p>
-<?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'silence' ); ?>
-</p>
-<!-- /wp:paragraph -->
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group">
+    <!-- wp:paragraph {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|30"}}},"textColor":"contrast-75"} -->
+    <p class="has-contrast-75-color has-text-color" style="margin-bottom:var(--wp--preset--spacing--30)"><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'silence' ); ?></p>
+    <!-- /wp:paragraph -->
 
-<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'label', 'silence' ); ?>","placeholder":"<?php echo esc_attr_x( 'Search...', 'placeholder for search field', 'silence' ); ?>","showLabel":false,"buttonText":"<?php esc_attr_e( 'Search', 'silence' ); ?>","buttonUseIcon":true} /-->
+    <!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'label', 'silence' ); ?>","showLabel":false,"placeholder":"<?php echo esc_attr_x( 'Search...', 'placeholder for search field', 'silence' ); ?>","buttonText":"<?php esc_attr_e( 'Search', 'silence' ); ?>"} /-->
+</div>
+<!-- /wp:group -->
